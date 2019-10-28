db42506502f6bd295ed4691e773d8700c6ef8be6
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2019-10-28 10:30:04 -0500

    #2 done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+
+// is this cheating?
+const { encrypt } = require('caesar-encrypt');
 const app = express();
 
 app.use(morgan('dev'));
@@ -8,6 +11,7 @@ app.get('/', (req, res) => {
   res.send('shit works');
 });
 
+//  #1
 app.get('/sum', (req, res) => {
   const a = parseInt(req.query.a);
   const b = parseInt(req.query.b);
@@ -18,6 +22,19 @@ app.get('/sum', (req, res) => {
 
   const sum = `${a + b}`;
   res.send(sum);
+});
+
+// #2
+app.get('/cipher', (req, res) => {
+  const text = req.query.text.toString();
+  const shift = req.query.shift;
+
+  if (!text || !shift) {
+    return res.status(400).send('Yuh fucked up kid');
+  }
+
+  const encrypted = encrypt(text, shift);
+  res.send(encrypted);
 });
 
 app.listen(9001, () => {
